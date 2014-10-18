7831bcc8f43387f10eb99ea74472945273621f50
unknown <irfanshk80@gmail.com>
2014-10-19 00:14:14 +0530

    added mark complete link to update the task as complete

--- protected/views/task/admin.php
+++ protected/views/task/admin.php
@@ -28,12 +28,7 @@ $('.search-form form').submit(function(){
 
 <h1>Manage Tasks</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
@@ -43,7 +38,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'task-grid',
 	'dataProvider'=>$model->search(),
-	'filter'=>$model,
+	//'filter'=>$model,
 	'columns'=>array(
 		'id',
 		'userId',
@@ -52,6 +47,10 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'marked',
 		array(
 			'class'=>'CButtonColumn',
+			'template'=> '{Mark_Complete} {delete}',
+			'buttons' => array(
+				'Mark_Complete' => array("url" => '$this->grid->controller->createUrl("task/update",array("id"=>$data->id))'),
+			),
 		),
 	),
 )); ?>
