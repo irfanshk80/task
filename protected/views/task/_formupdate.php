e61f9d18719cec6564069ff754798098df045ba6
unknown <irfanshk80@gmail.com>
2014-10-19 00:16:56 +0530

    added one more form for marking task as complete

---
+++ protected/views/task/_formupdate.php
@@ -0,0 +1,52 @@
+<?php
+/* @var $this TaskController */
+/* @var $model Task */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'task-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<!-- <div class="row">
+		<?php echo $form->labelEx($model,'userId'); ?>
+		<?php echo $form->textField($model,'userId'); ?>
+		<?php echo $form->error($model,'userId'); ?>
+	</div> -->
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>20,'maxlength'=>20)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'description'); ?>
+		<?php echo $form->textField($model,'description',array('size'=>30,'maxlength'=>30)); ?>
+		<?php echo $form->error($model,'description'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'marked'); ?>
+		<?php echo $form->checkBox($model,'marked',array('size'=>2,'maxlength'=>2)); ?>
+		<?php echo $form->error($model,'marked'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
